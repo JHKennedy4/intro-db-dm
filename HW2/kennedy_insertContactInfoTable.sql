86e95d44b4e69b870c467d55031947885a9f7bc4
John Kennedy <jhkennedy4@gmail.com>
2012-12-11 02:06:21 -0500

    insert script

---
+++ HW2/kennedy_insertContactInfoTable.sql
@@ -0,0 +1,8 @@
+# HW 2 insert
+
+INSERT INTO contact_info VALUES
+('Jacob', 'P', 'Jacobson', 'Jr.', 'Mr.', 'Director', 'Finance', 'rjameson@concor.com',
+'www.concor.com/~rjames', 'jpjacobson', '323-546-6834 ext. 29', 'work', 1969-07-13,
+'All meetings must be scheduled through Charlene Reynolds', 'Concor International, Inc.',
+'143 South Main Street', '', 'Los Angeles', 'CA', '90012-3712', 'USA', 'www.concor.com',
+'323-546-6834');
